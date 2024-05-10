91992de8cce518492aa8a9283d07176c51ceaba5
akamathb <ashwin_kamathb@intuit.com>
2024-05-10 12:03:12 +0530

    Clients

---
+++ Output/dinterfacepoc.h
@@ -0,0 +1,16 @@
+#ifndef __DINTERFACEPOC_H
+#define __DINTERFACEPOC_H
+
+#include <graal_isolate.h>
+
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+void list_annotated_class(graal_isolatethread_t*);
+
+#if defined(__cplusplus)
+}
+#endif
+#endif
---
+++ Output/dinterfacepoc_dynamic.h
@@ -0,0 +1,16 @@
+#ifndef __DINTERFACEPOC_H
+#define __DINTERFACEPOC_H
+
+#include <graal_isolate_dynamic.h>
+
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef void (*list_annotated_class_fn_t)(graal_isolatethread_t*);
+
+#if defined(__cplusplus)
+}
+#endif
+#endif
